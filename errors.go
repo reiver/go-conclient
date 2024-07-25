94c230e95de97f9e74c68ae96f2ccba213e7ebd4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-07-25 05:53:14 -0700

    errors

--- errors.go
+++ errors.go
@@ -7,6 +7,7 @@ import (
 const (
 	errMissingContractAddress = erorr.Error("conclient: missing contract-address")
 	errMissingRPCURL          = erorr.Error("conclient: missing rpc-url")
+	errNilCallData            = erorr.Error("conclient: nil call-data")
 	errNilReceiver            = erorr.Error("conclient: nil receiver")
 	errNilRPCClient           = erorr.Error("conclient: nil rpc-client")
 )
